d9da4813d959c6027b1df5036c0cd51b59359643
Amarjeet Sahoo <amarjeethappysahoo@gmail.com>
2020-05-28 17:22:03 +0530

    final commit

--- app/src/main/java/com/amarjeet/android/loginsignup/MainActivity.java
+++ app/src/main/java/com/amarjeet/android/loginsignup/MainActivity.java
@@ -2,13 +2,27 @@ package com.amarjeet.android.loginsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup=(TextView)findViewById(R.id.signin);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
